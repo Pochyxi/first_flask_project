c2a6f42ea99015fe85a53d3abb41f8e50d2579c4
Pochyxi <adienerlopez@gmail.com>
2023-02-07 23:37:07 +0100

    up

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
+# pip install flask-sqlalchemy per installare database
+from flask_sqlalchemy import SQLAlchemy
 
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+
+# initialize the app with the extension
+db.init_app(app)
+
+
+class Item(db.Model):
+    # id è la primary key ed è obbligatoriamente richiesta
+    id = db.Column(db.Integer(), primary_key=True)
+    # Column specifica che questa proprietà è una colonna della tabella
+    # String specifica il tipo di dato che viene accettato, lengh limita la lunghezza a 30 caratteri
+    # Nullable indica che questa proprietà non può eseere omessa
+    # Unique specifica che non possono esserci altri nomi uguali a questo
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=30), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 
 @app.route('/')
 @app.route('/home')
-def home():
+def home_page():
     return render_template('home.html')
 
 
