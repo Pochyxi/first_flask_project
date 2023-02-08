e1fd4a14d2eb05d5c50a6b27226041e8adea1dcd
Pochyxi <adienerlopez@gmail.com>
2023-02-08 01:19:02 +0100

    updt

--- market.py
+++ market.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template
 # pip install flask-sqlalchemy per installare database
 from flask_sqlalchemy import SQLAlchemy
-
-# create the extension
-db = SQLAlchemy()
-# create the app
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
-
-# initialize the app with the extension
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
 
 class Item(db.Model):
